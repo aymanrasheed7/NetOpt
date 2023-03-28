aae38d2c9c62e12ecc4777e043cf8df4795e231c
aymanrasheed7 <56726922+aymanrasheed7@users.noreply.github.com>
2023-03-29 02:42:50 +0600

    long double to double

--- NetOptThread.cpp
+++ NetOptThread.cpp
@@ -28,76 +28,73 @@ struct Node {
     long long numer, denom, nmask, childNumer, childDenom, childNmask;
     Node(long long a = 0, long long b = 1, long long c = 0,
         long long d = 0, long long e = 1, long long f = 0) {
-        b < 0 ? (b = -b, a = -a) : 0; e < 0 ? (e = -e, d = -d) : 0; numer = a;
-        denom = b; nmask = c; childNumer = d; childDenom = e; childNmask = f;
-        childNumer /= f = __gcd(abs(d), e); childDenom /= f;
-        numer /= c = __gcd(abs(a), b); denom /= c;
+        numer = a; denom = b; nmask = c; childNumer = d; childDenom = e;
+        childNmask = f; childNumer /= f = __gcd(d, e); childDenom /= f;
+        numer /= c = __gcd(a, b); denom /= c;
     } Node operator+(Node& a) {
         return Node(numer * a.denom + denom * a.numer, denom * a.denom,
             nmask | a.nmask, numer, denom, nmask);
     } Node operator|(Node& a) {
         return Node(numer * a.numer, numer * a.denom + denom * a.numer,
             nmask | a.nmask, numer, denom, nmask);
-    } bool operator<(long double a) { return numer < denom* a; }
+    } bool operator<(double a) { return numer < denom* a; }
     bool operator<(Node& a) { return numer * a.denom < denom* a.numer; }
     bool operator==(Node& a) { return numer * a.denom == denom * a.numer; }
-    long double operator~() { return childNumer / (long double)childDenom; }
-    long double operator!() { return numer / (long double)denom; }
-}; long long E12_SERIES[] = { 10,12,15,18,22,27,33,39,47,56,68,82,0 };
-long long ONE_SERIES[] = { 1,1,1,1,1,1,1,1,1,1,1,1,0 };
-long long INT_SERIES[] = { 1,2,3,4,5,6,7,8,9,10,11,12,0 };
-long long ODD_SERIES[] = { 1,3,5,7,9,11,13,15,17,19,21,23,0 };
-long long* series, denominator = 1, nT = 256, mask2id[8192];
-vector<Node> mini(8192), maxi(8192), table[8192];
-void recurse(Node& lo, Node& hi, long double target, long long mask) {
+    double operator~() { return childNumer / (childDenom + 0.0); }
+    double operator!() { return numer / (denom + 0.0); }
+}; long long E12_SERIES[] = { 10,12,15,18,22,27,33,39,47,56,68,82,0,0,0,0 };
+long long ONE_SERIES[] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 };
+long long INT_SERIES[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0 };
+long long ODD_SERIES[] = { 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,0 };
+long long nT = 256; Node mini[65536], maxi[65536]; vector<Node> table[65536];
+void recurse(Node& lo, Node& hi, double target, long long mask) {
     if (lo = mini[mask], hi = maxi[mask], mask == (mask & -mask)) return;
     Node temp, loch, hich; long long pos = 0, subMask = 0, compleMask = 0;
     function<void(Node&, Node)> upd = [&](Node& a, Node b) {
         abs(target - !b) < abs(target - !a) ? a = b : a; };
     if (target < !lo) hi = lo; else if (hi < target) lo = hi;
-    else if (!table[mask].empty()) pos = lower_bound(table[mask].begin(),
-        table[mask].end(), target) - table[mask].begin(), lo =
-        table[mask][pos - !!pos], hi = Node(table[mask][min(
-            pos, (long long)table[mask].size() - 1)]);
+    else if (!table[mask].empty()) pos = lower_bound(table[mask].begin(), table
+        [mask].end(), target) - table[mask].begin(), lo = table[mask][pos - !!
+        pos], hi = table[mask][pos - (pos == table[mask].size())];
     else for (subMask = mask; subMask = (subMask - 1) & mask;) if (!table
         [compleMask = mask ^ subMask].empty()) for (pos = table[compleMask]
             .size(); pos--; temp = table[compleMask][pos], recurse(loch, hich,
                 temp < target ? target - !temp : 1 / (1 / target - 1 / !temp),
-                subMask), upd(temp < target ? lo : hi, temp < target ? temp |
-                    maxi[subMask] : temp + mini[subMask]), upd(lo, temp <
-                        target ? temp + loch : temp | loch), upd(hi, temp
-                            < target ? temp + hich : temp | hich));
-} void solve(Node& lo, Node& hi, long double target, long long mask) {
-    if (lo = mini[mask], hi = maxi[mask], mask == (mask & -mask) &&
-        printf("%.1f", series[mask2id[mask]] / (denominator + 0.0))) return;
+                subMask), temp < target ? upd(lo, temp | maxi[subMask]) :
+            upd(hi, temp + mini[subMask]), upd(lo, temp < target ? temp
+                + loch : temp | loch), upd(hi, temp < target ?
+                    temp + hich : temp | hich));
+} void solve(Node& lo, Node& hi, double target, long long mask) {
+    if (lo = mini[mask], hi = maxi[mask], mask == (mask & -mask)
+        && printf("%.1f", !lo)) return;
     Node temp, loch, hich; long long pos = 0, subMask = 0, compleMask = 0;
     function<void(Node&, Node)> upd = [&](Node& a, Node b) {
         abs(target - !b) < abs(target - !a) ? a = b : a; };
     if (target < !lo) hi = lo; else if (hi < target) lo = hi;
     else if (table[mask].empty()) {
-        mutex mtx; long long rJ = 0, nJ = 0; vector<thread> threads(nT);
+        mutex mtx; thread threads[nT]; long long rJ, nJ = 0;
         for (subMask = mask; subMask = (subMask - 1) & mask;
             nJ += table[subMask].size());
         vector<long long> qMask(rJ = nJ);
-        vector<Node> qNode(nJ), qlo(nJ), qhi(nJ);
+        vector<Node> qNode(nJ), qLo(nJ), qHi(nJ);
         for (subMask = mask; subMask = (subMask - 1) & mask;)
             if (!table[compleMask = mask ^ subMask].empty()) for (pos =
-                table[compleMask].size(); pos--; qMask[--rJ] = subMask,
-                qNode[rJ] = table[compleMask][pos]); rJ = nJ;
+                table[compleMask].size(); pos--; qMask[--nJ] = subMask,
+                qNode[nJ] = table[compleMask][pos]);
         function<void()> par = [&]() {
             for (long long i; mtx.lock(), i = --rJ, mtx.unlock(), 0 <= i;
-                recurse(qlo[i], qhi[i], qNode[i] < target ? target - !qNode[i]
-                    : 1 / (1 / target - 1 / !qNode[i]), qMask[i])); };
+                recurse(qLo[i], qHi[i], qNode[i] < target ? target - !qNode[i]
+                    : 1 / (1 / target - 1 / !qNode[i]), qMask[i]), mtx.lock(),
+                qNode[i] < target ? upd(lo, qNode[i] | maxi[qMask[i]]) :
+                upd(hi, qNode[i] + mini[qMask[i]]), upd(lo, qNode[i] <
+                    target ? qNode[i] + qLo[i] : qNode[i] | qLo[i]),
+                upd(hi, qNode[i] < target ? qNode[i] + qHi[i] :
+                    qNode[i] | qHi[i]), mtx.unlock()); };
         for (long long i = nT; i--; threads[i] = thread(par));
         for (long long i = nT; i--; threads[i].join());
-        for (; nJ--; upd(qNode[nJ] < target ? lo : hi, qNode[nJ] < target ?
-            qNode[nJ] | maxi[qMask[nJ]] : qNode[nJ] + mini[qMask[nJ]]),
-            upd(lo, qNode[nJ] < target ? qNode[nJ] + qlo[nJ] :
-                qNode[nJ] | qlo[nJ]), upd(hi, qNode[nJ] < target
-                    ? qNode[nJ] + qhi[nJ] : qNode[nJ] | qhi[nJ]));
     } else pos = lower_bound(table[mask].begin(), table[mask].end(),
         target) - table[mask].begin(), lo = table[mask][pos - !!pos],
-        hi = Node(table[mask][min(pos, (long long)table[mask].size() - 1)]);
+        hi = table[mask][pos - (pos == table[mask].size())];
     abs(target - !hi) < abs(target - !lo) ? temp = hi : temp = lo;
     printf("("); solve(loch, hich, ~temp, temp.childNmask); printf(!temp <
         ~temp ? "|" : "+"); solve(loch, hich, !temp < ~temp ? 1 / (1 / !temp
@@ -105,26 +102,28 @@ void recurse(Node& lo, Node& hi, long double target, long long mask) {
 } int main(int argc, char** argv) {
     chrono::_V2::system_clock::time_point begin
         = chrono::high_resolution_clock::now();
-    series = E12_SERIES; Node lo, hi; long double target = sqrtl(2);
+    Node lo, hi; double target = sqrt(2);
+    long long* series = E12_SERIES, denominator = 1;
     long long nR = atoi(argv[3]), tableSize = nR + 1 >> 1, maskSize = 1 << nR;
-    if (4 < argc) tableSize = atoi(argv[4]); if (5 < argc) nT = atoi(argv[5]);
+    if (4 < argc) tableSize = max(tableSize, (long long)atoi(argv[4]));
+    if (5 < argc) nT = atoi(argv[5]);
     if (!strcmp(argv[1], "E12")) denominator = 10;
     else if (!strcmp(argv[1], "ONE")) series = ONE_SERIES;
     else if (!strcmp(argv[1], "INT")) series = INT_SERIES;
     else if (!strcmp(argv[1], "ODD")) series = ODD_SERIES;
-    if (!strcmp(argv[2], "E")) target = expl(1);
-    else if (!strcmp(argv[2], "PI")) target = acosl(-1);
-    else if (!strcmp(argv[2], "PHI")) target = sqrtl(5) * 0.5 + 0.5;
-    else if (!strcmp(argv[2], "SQRT")) target = sqrtl(nR);
-    else if (!strncmp(argv[2], "SQRT", 4)) target = sqrtl(stold(argv[2] + 4));
-    else target = stold(argv[2]), target < 0 ? (target = -target,
-        series[nR++] = 0, maskSize = 1 << nR, tableSize = nR + 1 >> 1) : 0;
+    if (!strcmp(argv[2], "E")) target = exp(1);
+    else if (!strcmp(argv[2], "PI")) target = acos(-1);
+    else if (!strcmp(argv[2], "PHI")) target = sqrt(5) * 0.5 + 0.5;
+    else if (!strcmp(argv[2], "SQRT")) target = sqrt(nR);
+    else if (!strncmp(argv[2], "SQRT", 4)) target = sqrt(stod(argv[2] + 4));
+    else target = stod(argv[2]), target < 0 ? (target = -target, series[nR++]
+        = 0, maskSize = 1 << nR, tableSize = max(tableSize, nR + 1 >> 1)) : 0;
     for (long long mask = 0, subMask, compleMask, i, j; maskSize > ++mask; sort(
         table[mask].begin(), table[mask].end()), table[mask].resize(unique(table
             [mask].begin(), table[mask].end()) - table[mask].begin()))
-        if (mask == (mask & -mask) && (table[mask].push_back(mini
-            [mask] = maxi[mask] = Node(series[mask2id[mask] =
-                __builtin_ctzll(mask)], denominator, mask)), 1));
+        if (mask == (mask & -mask) && (table[mask].push_back(mini[mask] =
+            maxi[mask] = Node(series[__builtin_ctzll(mask)],
+                denominator, mask)), 1));
         else if (mini[mask] = mini[mask & -mask] | mini[mask ^ (mask & -mask)],
             maxi[mask] = maxi[mask & -mask] + maxi[mask ^ (mask & -mask)],
             __builtin_popcountll(mask) <= tableSize)
@@ -136,8 +135,8 @@ void recurse(Node& lo, Node& hi, long double target, long long mask) {
                             table[compleMask][j]));
     printf("Solution: "), solve(lo, hi, target, maskSize - 1);
     abs(target - !hi) < abs(target - !lo) ? (swap(lo, hi), 0) : 0;
-    printf("\nResult: %.16lf (%lld/%lld)\nTarget: %.16lf\nCost: %le\n", (double)
-        !lo, lo.numer, lo.denom, (double)target, (double)abs(target - !lo));
+    printf("\nResult: %.16lf (%lld/%lld)\nTarget: %.16lf\nCost: %le\n",
+        !lo, lo.numer, lo.denom, target, abs(target - !lo));
     printf("CPU time (s): %.9lf\n", clock() / (CLOCKS_PER_SEC + 0.0));
     chrono::_V2::system_clock::time_point end
         = chrono::high_resolution_clock::now();
